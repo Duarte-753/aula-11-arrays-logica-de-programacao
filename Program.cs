40b0b3425dc8ff7c76dcb47cf0c723dfc7d957e6
curso.ads <curso.ads@LAB06-06>
2022-06-01 20:46:18 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_11_arrays_logica_de_programacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CRUD - Insert ,Select , Update, Delete.
+
+
+            /*   (    i=0           ;    i<5  ;  i++)
+            for  (variavel numerica; condição; passo) {}
+
+            while (Condição) {}
+                  (  a<5   )
+
+            do {} while(condição)
+                       (  i<5   )
+
+            */
+        }
+    }
+}
